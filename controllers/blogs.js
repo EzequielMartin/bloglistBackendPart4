29465443cd9c29b741c6e2273809033a19dcd685
Ezequiel Martin <ezequielmartin92@gmail.com>
2025-04-05 19:09:31 -0300

    Hago las modificaciones necesarias para que los blogs y los usuarios esten asociados e implemento el request para agregar blogs con usuario asociado

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,8 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blogs")
 
+const User = require("../models/user")
+
 blogsRouter.get("/", (request, response) => {
   Blog
     .find({})
@@ -9,21 +11,41 @@ blogsRouter.get("/", (request, response) => {
     })
 })
 
-blogsRouter.post("/", (request, response) => {
-  let blog = new Blog(request.body)
+blogsRouter.post("/", async (request, response) => {
+  //let blog = new Blog(request.body)
 
-  if(blog.likes === undefined){
-    blog.likes = 0
-  }
-  if(blog.title === undefined || blog.url === undefined){
-    return response.status(400)
-  }
+  const body = request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const user = await User.findById(body.userId)
+
+  let blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id
+  })
+
+  //Ver como implementar esto usando async/await, ya que no deberia mezclar async/await con .then()
+
+  // if(blog.likes === undefined){
+  //   blog.likes = 0
+  // }
+  // if(blog.title === undefined || blog.url === undefined){
+  //   return response.status(400)
+  // }
+
+  // blog
+  //   .save()
+  //   .then(result => {
+  //     response.status(201).json(result)
+  //   })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete("/:id", (request, response) => {
