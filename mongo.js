1319ca064f40b6ba52066412f4d6ee2dbe7a9a1e
Ezequiel Martin <ezequielmartin92@gmail.com>
2025-04-05 02:24:03 -0300

    Modifico el schema de Blog y agrego un test para probar que el identificador unico de los blogs se llama id

--- mongo.js
+++ mongo.js
@@ -22,14 +22,22 @@ mongoose.set("strictQuery",false)
 
 mongoose.connect(uri)
 
-const blogSchemna = new mongoose.Schema({
+const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
 })
 
-const Blog = mongoose.model("Blog", blogSchemna)
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Blog = mongoose.model("Blog", blogSchema)
 
 const blog = new Blog({
   title: `${title}`,
@@ -41,6 +49,7 @@ const blog = new Blog({
 if(process.argv.length === 2){
   Blog.find({}).then(result => {
     result.forEach(blog => {
+      console.log(blog)
       console.log(`${blog.title} ${blog.author} ${blog.url} ${blog.likes}`)
     })
     mongoose.connection.close()
--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -43,7 +43,7 @@ beforeEach(async () => {
 
 //usando async/await
 test("get returns the amount of blogs defined in initialBlogs"), async () => {
-  const response = await api.get("/api/notes")
+  const response = await api.get("/api/blogs")
 
   assert.strictEqual(response.body.length, initialBlogs.length)
 
@@ -54,6 +54,14 @@ test("blogs are returned as json", async () => {
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/)
+})
+
+test("verify id property name", async () => {
+  const response = await api.get("/api/blogs")
+
+  response.body.forEach(blog => {
+    assert.strictEqual(Object.prototype.hasOwnProperty.call(blog, "id"), true)
+  })
 })
 
 after(async () => {
