3a07b2e1b1f8a6238355695bab23bd78fbefac21
Ezequiel Martin <ezequielmartin92@gmail.com>
2025-04-05 00:45:40 -0300

    Agrego tests

--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -1,9 +1,17 @@
 const { test, after } = require("node:test")
+const assert = require("node:assert")
 const mongoose = require("mongoose")
 const supertest = require("supertest")
 const app = require("../app")
 
 const api = supertest(app)
+
+test("get returns 2 blogs"), async () => {
+  const response = await api.get("/api/notes")
+
+  assert.strictEqual(response.body.length, 2)
+
+}
 
 test("blogs are returned as json", async () => {
   await api
