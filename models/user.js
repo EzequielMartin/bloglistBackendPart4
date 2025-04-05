3223b7d8c10a00d45511c98c72893778cab428fb
Ezequiel Martin <ezequielmartin92@gmail.com>
2025-04-05 18:01:51 -0300

    Corrijo el esquema de user

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
   ]
 })
 
-userSchema.set("toJson", {
+userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
